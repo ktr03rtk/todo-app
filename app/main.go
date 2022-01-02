275d84f6af7a577e9da994136c900d02bc7a4662
ktr03 <ktrsouth@gmail.com>
2022-01-02 13:50:07 +0900

    fail fmt

--- app/main.go
+++ app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "fist")
+	  fmt.Fprintf(w, "fist")
 }
 
 func secondHandler(w http.ResponseWriter, r *http.Request) {
