a43973fb1bbb1eeeb09fff92614c0b22cd579d74
ktr03 <ktrsouth@gmail.com>
2022-01-22 23:34:42 +0900

    feature: task register usecase

---
+++ app/usecase/task_usecase/task_register_usecase.go
@@ -0,0 +1,37 @@
+package task_usecase
+
+import (
+	"time"
+	"todo-app/domain/model/task_model"
+	"todo-app/domain/model/uuid_model"
+	"todo-app/domain/repository/task_repository"
+
+	"github.com/pkg/errors"
+)
+
+type TaskRegisterUsecase interface {
+	Execute(name, detail string, deadline time.Time) error
+}
+
+type taskRegisterUsecase struct {
+	taskRepository task_repository.TaskRepository
+}
+
+func NewTaskRegisterUsecase(tr task_repository.TaskRepository) TaskRegisterUsecase {
+	return &taskRegisterUsecase{taskRepository: tr}
+}
+
+func (u *taskRegisterUsecase) Execute(name, detail string, deadline time.Time) error {
+	id := uuid_model.CreateUUID()
+
+	t, err := task_model.CreateTask(task_model.TaskID(id), name, detail, deadline)
+	if err != nil {
+		return errors.Wrap(err, "failed to create task")
+	}
+
+	if err := u.taskRepository.Insert(*t); err != nil {
+		return errors.Wrap(err, "failed to store task")
+	}
+
+	return nil
+}
---
+++ app/usecase/task_usecase/task_register_usecase_test.go
@@ -0,0 +1,71 @@
+package task_usecase
+
+import (
+	"fmt"
+	"testing"
+	"time"
+	"todo-app/mock"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTaskRegisterUseCase(t *testing.T) {
+	tests := []struct {
+		name     string
+		taskName string
+		detail   string
+		deadline time.Time
+		// expectedInput task_model.Task
+		expectedOutput    error
+		expectedErr       string
+		expectedCallTimes int
+	}{
+		{
+			"normal case",
+			"Venue Reservation",
+			"Reserve venue for conference",
+			time.Now().AddDate(0, 0, 1),
+			nil,
+			"",
+			1,
+		},
+		{
+			"error case(deadline is past)",
+			"Venue Reservation",
+			"Reserve venue for conference",
+			time.Now().AddDate(0, 0, -1),
+			nil,
+			"past day is set on deadline",
+			0,
+		},
+		{
+			"error case(fail to store)",
+			"Venue Reservation",
+			"Reserve venue for conference",
+			time.Now().AddDate(0, 0, 1),
+			fmt.Errorf("fail to store"),
+			"fail to store",
+			1,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt // https://github.com/golang/go/wiki/CommonMistakes#using-goroutines-on-loop-iterator-variables
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			taskRepository := mock.NewMockTaskRepository(ctrl)
+			usecase := NewTaskRegisterUsecase(taskRepository)
+
+			taskRepository.EXPECT().Insert(gomock.Any()).Return(tt.expectedOutput).Times(tt.expectedCallTimes)
+
+			if err := usecase.Execute(tt.taskName, tt.detail, tt.deadline); err != nil {
+				assert.Contains(t, err.Error(), tt.expectedErr)
+			} else {
+				assert.Exactly(t, tt.expectedErr, "", "error is expected but received nil")
+			}
+		})
+	}
+}
