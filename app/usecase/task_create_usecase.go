9b0e9ce043132329a0dcaebda1fc3b34decb84d0
ktr03 <ktrsouth@gmail.com>
2022-01-27 22:36:09 +0900

    rename usecase

--- app/usecase/task_create_usecase.go
+++ app/usecase/task_create_usecase.go
@@ -8,19 +8,19 @@ import (
 	"github.com/pkg/errors"
 )
 
-type TaskRegisterUsecase interface {
+type TaskCreateUsecase interface {
 	Execute(name, detail string, deadline time.Time) error
 }
 
-type taskRegisterUsecase struct {
+type taskCreateUsecase struct {
 	taskRepository repository.TaskRepository
 }
 
-func NewTaskRegisterUsecase(tr repository.TaskRepository) TaskRegisterUsecase {
-	return &taskRegisterUsecase{taskRepository: tr}
+func NewTaskCreateUsecase(tr repository.TaskRepository) TaskCreateUsecase {
+	return &taskCreateUsecase{taskRepository: tr}
 }
 
-func (u *taskRegisterUsecase) Execute(name, detail string, deadline time.Time) error {
+func (u *taskCreateUsecase) Execute(name, detail string, deadline time.Time) error {
 	id := model.CreateUUID()
 
 	t, err := model.CreateTask(model.TaskID(id), name, detail, deadline)
