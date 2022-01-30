b783a83e6c6a8451f1179614cd02ad5a43574bdc
ktr03 <ktrsouth@gmail.com>
2022-01-30 19:47:15 +0900

    feature: view task detail

--- app/go.mod
+++ app/go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- app/interfaces/handler/handler.go
+++ app/interfaces/handler/handler.go
@@ -2,9 +2,13 @@ package handler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
+	"todo-app/domain/model"
 	"todo-app/usecase"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 type Handler interface {
@@ -20,17 +24,15 @@ func NewHandler(u usecase.TaskUsecase) Handler {
 }
 
 func (h *handler) Start() {
-	server := http.Server{
-		Addr: "0.0.0.0:8080",
-	}
-
-	http.HandleFunc("/task", h.taskFindAll)
+	router := httprouter.New()
+	router.GET("/task", h.findAllTask)
+	router.GET("/task/:id", h.findTask)
 
 	fmt.Println("server start")
-	server.ListenAndServe()
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func (h *handler) taskFindAll(w http.ResponseWriter, r *http.Request) {
+func (h *handler) findAllTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	tasks, err := h.taskUsecase.FindAll()
 	if err != nil {
 		fmt.Fprintf(w, "error")
@@ -38,10 +40,28 @@ func (h *handler) taskFindAll(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{"templates/layout.html", "templates/task.html"}
+	files := []string{"templates/layout.html", "templates/task_all.html"}
 	templates := template.Must(template.ParseFiles(files...))
 
 	if err := templates.ExecuteTemplate(w, "layout", tasks); err != nil {
+		fmt.Fprintf(w, "error")
+	}
+}
+
+func (h *handler) findTask(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id := model.TaskID(ps.ByName("id"))
+
+	task, err := h.taskUsecase.FindByID(id)
+	if err != nil {
+		fmt.Fprintf(w, "error")
+
+		return
+	}
+
+	files := []string{"templates/layout.html", "templates/task_detail.html"}
+	templates := template.Must(template.ParseFiles(files...))
+
+	if err := templates.ExecuteTemplate(w, "layout", task); err != nil {
 		fmt.Fprintf(w, "error")
 	}
 }
