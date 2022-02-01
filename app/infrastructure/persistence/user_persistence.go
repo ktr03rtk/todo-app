e0d242eb75796cdb16ab24642e9dcaf7be80ac1d
ktr03 <ktrsouth@gmail.com>
2022-02-02 01:14:11 +0900

    infrastructure: user

---
+++ app/infrastructure/persistence/user_persistence.go
@@ -0,0 +1,37 @@
+package persistence
+
+import (
+	"todo-app/domain/model"
+	"todo-app/domain/repository"
+
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+)
+
+type UserPersistence struct {
+	conn *gorm.DB
+}
+
+func NewUserPersistence(conn *gorm.DB) repository.UserRepository {
+	return &UserPersistence{
+		conn,
+	}
+}
+
+func (up *UserPersistence) Create(user *model.User) error {
+	if err := up.conn.Create(&user).Error; err != nil {
+		return errors.Wrapf(err, "failed to create user. user email: %+v", &user.Email)
+	}
+
+	return nil
+}
+
+func (up *UserPersistence) FindByID(email model.Email) (*model.User, error) {
+	t := &model.User{Email: email}
+
+	if err := up.conn.First(&t).Error; err != nil {
+		return nil, errors.Wrapf(err, "failed to find user. user email: %+v", t.Email)
+	}
+
+	return t, nil
+}
