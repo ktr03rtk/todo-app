3566592acdb5d20dd299b8b153d61ec274425701
ktr03 <ktrsouth@gmail.com>
2022-02-05 19:20:40 +0900

    fix query

--- app/infrastructure/persistence/session_persistence.go
+++ app/infrastructure/persistence/session_persistence.go
@@ -41,7 +41,7 @@ func (up *SessionPersistence) FindByID(id usecase.SessionID) (*usecase.Session, 
 func (up *SessionPersistence) FindByUserID(id model.UserID) (*usecase.Session, error) {
 	s := &usecase.Session{UserID: id}
 
-	if err := up.conn.Where(&s).First(&s).Error; errors.Is(err, gorm.ErrRecordNotFound) {
+	if err := up.conn.Where("user_id = ?", s.UserID).First(&s).Error; errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, nil
 	} else if err != nil {
 		return nil, errors.Wrapf(err, "failed to find session. user id: %+v", id)
@@ -53,8 +53,8 @@ func (up *SessionPersistence) FindByUserID(id model.UserID) (*usecase.Session, e
 func (up *SessionPersistence) Delete(id model.UserID) error {
 	s := &usecase.Session{UserID: id}
 
-	if err := up.conn.Where(&s).Delete(&s).Error; err != nil {
-		return errors.Wrapf(err, "failed to create session. session id: %+v", id)
+	if err := up.conn.Where("user_id = ?", s.UserID).Delete(&s).Error; err != nil {
+		return errors.Wrapf(err, "failed to delete session. session id: %+v", id)
 	}
 
 	return nil
